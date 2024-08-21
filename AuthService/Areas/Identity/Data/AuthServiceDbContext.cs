e94cc28e067cc7f7594d6349bf8179eda37e4554
Akhmed Mirsaitov <bignumbergames@gmail.com>
2024-08-21 19:56:37 +0300

    Добавил identity

---
+++ AuthService/Areas/Identity/Data/AuthServiceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AuthService.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data;
+
+public class AuthServiceDbContext : IdentityDbContext<AuthServiceUser>
+{
+    public AuthServiceDbContext(DbContextOptions<AuthServiceDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AuthService.Data;
+using AuthService.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AuthServiceDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthServiceDbContextConnection' not found.");
+
+builder.Services.AddDbContext<AuthServiceDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services.AddDefaultIdentity<AuthServiceUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AuthServiceDbContext>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
